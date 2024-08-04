67303fe8910cc0a0a9d6845dd7dad179415e076f
klakli <kkborowy@gmail.com>
2024-08-04 13:28:20 +0200

    find_bucket_names function

--- airflow/tasks/uploader/uploader.py
+++ airflow/tasks/uploader/uploader.py
@@ -1,18 +1,24 @@
 import boto3
 
-# TODO find a name of our bucket
-bucket_name = 'idk'
+def find_bucket_names(prefix: str) -> list:
+    client = boto3.client('s3')
+    response = client.list_buckets()
+    bucket_names = [bucket['Name'] for bucket in response['Buckets']]
+    matching = [name for name in bucket_names if name.startswith(prefix)]
+    return(matching)
 
-def upload_to_s3(b_name: str, file_name: str, path: str):
+def upload_to_s3(bucket_name: str, file_name: str, path: str) -> None:
     s3 = boto3.resource('s3')
     try:
-        s3.Bucket(b_name).upload_file(path, file_name)
+        s3.Bucket(bucket_name).upload_file(path, file_name)
     except Exception as e:
-        print(f'Failed to upload {file_name} so S3, because of error: {e}')
+        print(f'Failed to upload {file_name} so S3, because of an error: {e}')
 
 if __name__ == '__main__':
+    prefix = 'pipeline-bucket-'  #TODO - get from a tf output
+    bucket_name = find_bucket_names(prefix)[0]
     upload_to_s3(
-        b_name = bucket_name,
+        bucket_name = bucket_name,
         file_name = 'bronze_data.csv',
         path = '/tmp/bronze_data.csv'
     )
