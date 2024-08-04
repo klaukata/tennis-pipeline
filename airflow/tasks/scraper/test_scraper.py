0c4639c04e569a98a2e6d58c4d2202914aca8f98
klakli <kkborowy@gmail.com>
2024-08-04 18:37:43 +0200

    test_fetch_website func

--- airflow/tasks/scraper/test_scraper.py
+++ airflow/tasks/scraper/test_scraper.py
@@ -3,6 +3,9 @@ import scraper
 from unittest.mock import MagicMock, patch
 
 class TestScraper(unittest.TestCase):
+    def setUp(self):
+        self.url = 'https://www.ultimatetennisstatistics.com/goatList'
+
     @patch('scraper.Options')
     @patch('scraper.Service')
     @patch('scraper.webdriver.Chrome')
@@ -20,7 +23,21 @@ class TestScraper(unittest.TestCase):
             options = options_val
         )
 
-    # TODO - test 4 widow size
+    def test_fetch_website(self):
+        """
+        Tests:
+            1. if a set_window_size() method is called with correct args
+            2. if a driver fetches with a correct url
+            3. if a func returns a driver
+        """
+        m_driver = MagicMock()
+        
+        result = scraper.fetch_website(m_driver)
+
+        result.set_window_size.assert_called_once_with(1980, 1080)
+        result.get.assert_called_once_with(self.url)
+        self.assertEqual(result, m_driver)
+
     # TODO - test 4 get page source == html
     # TODO - test 4 correctly indentified html emements to click
     # TODO - output of get_page_src == html
