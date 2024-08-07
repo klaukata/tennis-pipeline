263bc65073011d3093826384340ba136bb8c387a
klakli <kkborowy@gmail.com>
2024-08-07 22:59:55 +0200

    sns topic + subscripton

--- readme.md
+++ readme.md
@@ -3,6 +3,7 @@
 Set up (so far):
 
 1. obtain credentials, set them using `aws configure`
-2. `make infra`
+2. `make s3`
 3. `make outputs`
 4. run .py scripts (saving scraped data do s3 bucket)
+5. `make ft` (create a cloudwatch alarm)
--- terraform/s3.tf
+++ terraform/s3.tf
@@ -3,7 +3,7 @@ resource "aws_s3_bucket" "raw_data" {
 }
 
 resource "aws_cloudwatch_metric_alarm" "size_alarm" {
-    alarm_name =    "dfsd"
+    alarm_name =    "bucket_size_alarm"
     alarm_description = "Alarm when S3 bucket size exceeds "
     namespace =     "AWS/S3"
     metric_name =  "BucketSizeBytes"
@@ -16,6 +16,15 @@ resource "aws_cloudwatch_metric_alarm" "size_alarm" {
     comparison_operator = "GreaterThanOrEqualToThreshold"
     threshold = 50000 # 50kB
     evaluation_periods = 1 # condition must be met 1 time for an alarm to go off
-    alarm_actions = [  ]
+    alarm_actions = [aws_sns_topic.size_reached.arn]
 }
 
+resource "aws_sns_topic" "size_reached" {
+    name="bucket_size_topic"
+}
+
+resource "aws_sns_topic_subscription" "size_reached_subsciption" {
+    topic_arn = aws_sns_topic.size_reached.arn
+    protocol = "email"
+    endpoint = "kkborowy@gmail.com" #TODO - custom
+}
