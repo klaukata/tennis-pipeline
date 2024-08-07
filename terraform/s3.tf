d4d47e0162c0b1938dc66b2044be680edbc41811
klakli <kkborowy@gmail.com>
2024-08-07 21:33:19 +0200

    cloudwatch alarm with empty alarm_actions

--- terraform/s3.tf
+++ terraform/s3.tf
@@ -1,4 +1,21 @@
-resource "aws_s3_bucket" "bronze_data" {
+resource "aws_s3_bucket" "raw_data" {
     bucket_prefix = var.s3_prefix
-  
 }
+
+resource "aws_cloudwatch_metric_alarm" "size_alarm" {
+    alarm_name =    "dfsd"
+    alarm_description = "Alarm when S3 bucket size exceeds "
+    namespace =     "AWS/S3"
+    metric_name =  "BucketSizeBytes"
+    dimensions = {
+        StorageType = "StandardStorage"
+        BucketName = local.bucket_name
+    }
+    statistic = "Maximum"
+    period = 86400 # 1 day
+    comparison_operator = "GreaterThanOrEqualToThreshold"
+    threshold = 50000 # 50kB
+    evaluation_periods = 1 # condition must be met 1 time for an alarm to go off
+    alarm_actions = [  ]
+}
+
