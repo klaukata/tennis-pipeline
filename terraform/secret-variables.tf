9276771926fbdf8ecc4d69f2834655a497147370
klakli <kkborowy@gmail.com>
2024-08-09 17:10:19 +0200

    variables 4 snowflake credentials

---
+++ terraform/secret-variables.tf
@@ -0,0 +1,33 @@
+# change the default value to your own snowflake log-in credentials
+
+variable "snowflake_account" {
+  description   = "The Snowflake account for resources to be loaded into"
+  type          = string
+  default       = ""
+}
+
+variable "snowflake_region" {
+  description   = "The AWS region of the Snowflake account"
+  type          = string
+  default       = ""
+}
+
+variable "snowflake_username" {
+  description   = "The username for the Snowflake user"
+  sensitive     = true
+  type          = string
+  default       = ""
+}
+
+variable "snowflake_user_password" {
+  description   = "The password for the Snowflake user"
+  sensitive     = true
+  type          = string
+  default       = ""
+}
+
+variable "snowflake_user_role" {
+  description   = "The role of the Terraform user"
+  type          = string
+  default       = ""
+}
