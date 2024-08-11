d4af8b38b58dfbb486d0b35f495b28590ee5dafd
klakli <kkborowy@gmail.com>
2024-08-11 18:50:51 +0200

    reating an iam role + trust policy

---
+++ terraform/aws_iam.tf
@@ -0,0 +1,20 @@
+# data source for an acces to authorized account id
+data "aws_caller_identity" "current" {}
+
+# trust policy - data source that specifies who can assume the 'snowflake_uploader' role
+data "aws_iam_policy_document" "iam_trust_policy" {
+    statement {
+      actions = [ "sts:AssumeRole" ]
+      principals {
+        type = "AWS"
+        identifiers = "arn:aws:iam:${data.aws_caller_identity.current.account_id}:root"
+      }
+    }
+}
+
+# resource that creates a role
+resource "aws_iam_role" "snowflake_uploader" {
+    name = "snowflake_uploader"
+    description = "Role that will upload the most recent .csv file to Snowflake"
+    assume_role_policy = data.aws_iam_policy_document.iam_trust_policy
+}
