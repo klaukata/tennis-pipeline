475b00582f4a701fb202b392df984d00560acd8d
klakli <kkborowy@gmail.com>
2024-08-13 19:21:23 +0200

    create a storage integration

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -2,3 +2,8 @@ output "s3_bucket_prefix" {
     description = "A prefix of our raw data bucket"
     value = var.s3_prefix
 }
+
+output "itegration_description" {
+  description = "The SnowSQL query result from the read statements."
+  value = jsondecode(nonsensitive(snowsql_exec.read_integration_description.read_results))
+}
--- terraform/snowflake.tf
+++ terraform/snowflake.tf
@@ -1,6 +1,31 @@
 # creare a warehouse 
 resource "snowflake_warehouse" "wh" {
-  name = "most_recent_wh"
+  name = "project_wh"
   comment = "A warehouse for our most recent file"
   warehouse_size = "x-small"
+}
+
+# create a storage integration w/ a bucket
+resource "snowflake_storage_integration" "integration" {
+  name = "storage"
+  type = "EXTERNAL_STAGE"
+  storage_provider = "S3"
+  enabled = true
+  storage_aws_role_arn = "arn:aws:iam::${local.account_id}:role/${local.role_name}"
+  storage_allowed_locations = [ "s3://${local.bucket_name}/" ]
+}
+
+# read a description of this integration
+resource "snowsql_exec" "read_integration_description" {
+    create {
+      statements = "CREATE ROLE if not exists my_role"
+    }
+
+    read {
+      statements = "desc integration integ"
+    }
+
+    delete {
+      statements = "DROP ROLE IF EXISTS my_role"
+    }
 }
