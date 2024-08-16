5e4d8ca183c243bb1101e69c26e8b9bb9f424c6d
klakli <kkborowy@gmail.com>
2024-08-16 16:31:09 +0200

    update

--- terraform/snowflake.tf
+++ terraform/snowflake.tf
@@ -1,24 +1,11 @@
 # ____________WAREHOUSE____________
 resource "snowflake_warehouse" "wh" {
-  name = "project_wh"
+  name = "PROJECT_WH"
   warehouse_size = "x-small"
 }
 
 #____________DATABASE____________
 resource "snowflake_database" "goats" {
-  name = "goats"
+  name = "GOATS"
   comment = "A database containing both recent and historical versions of a GOAT data"
 }
-
-#____________SCHEMAS____________
-resource "snowflake_schema" "recent" {
-  name = "recent_schema"
-  database = "goats"
-}
-
-resource "snowflake_schema" "historical" {
-  name = "historical_schema"
-  database = "goats"
-}
-
-#____________ROLE____________
