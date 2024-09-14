37b9d07707a4d4576a0c9a721d807921763c6467
klakli <kkborowy@gmail.com>
2024-09-14 13:00:49 +0200

    aws_account_id and aws_role_name outputs

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -1,9 +1,16 @@
 output "s3_bucket_prefix" {
-    description = "A prefix of our raw data bucket"
-    value = var.s3_prefix
+  description = "A prefix of our raw data bucket"
+  value       = var.s3_prefix
 }
 
-output "itegration_description" {
-  description = "The SnowSQL query result from the read statements."
-  value = jsondecode(nonsensitive(snowsql_exec.read_integration_description.read_results))
+output "aws_account_id" {
+  value = module.m_02_aws.account_id
 }
+
+output "aws_role_name" {
+  value = module.m_02_aws.role_name
+}
+
+# output "snowflake_itegration_description" {
+#   value = module.m_03_sf_aws.itegration_description
+# }
