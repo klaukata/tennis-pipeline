99ac538a87bff613f3ab00cb5f31a09ee71d4eee
klakli <kkborowy@gmail.com>
2024-09-15 08:47:02 +0200

    snowflake_user_role as a global var

--- terraform/secret-variables.tf
+++ terraform/secret-variables.tf
@@ -1,27 +1,21 @@
 # change the default value to your own snowflake log-in credentials
 
 variable "snowflake_account" {
-  description   = "The Snowflake account for resources to be loaded into"
-  type          = string
-  default       = ""
+  description = "The Snowflake account for resources to be loaded into"
+  type        = string
+  default     = ""
 }
 
 variable "snowflake_user" {
-  description   = "The username for the Snowflake user"
-  sensitive     = true
-  type          = string
-  default       = ""
+  description = "The username for the Snowflake user"
+  sensitive   = true
+  type        = string
+  default     = ""
 }
 
 variable "snowflake_user_password" {
-  description   = "The password for the Snowflake user"
-  sensitive     = true
-  type          = string
-  default       = ""
-}
-
-variable "snowflake_user_role" {
-  description   = "The role of the Terraform user"
-  type          = string
-  default       = ""
+  description = "The password for the Snowflake user"
+  sensitive   = true
+  type        = string
+  default     = ""
 }
--- terraform/variables.tf
+++ terraform/variables.tf
@@ -4,6 +4,12 @@ variable "s3_prefix" {
   default     = "pipeline-bucket-"
 }
 
+variable "snowflake_user_role" {
+  description = "The role of the Terraform user, change before set up (if needed)"
+  type        = string
+  default     = "SYSADMIN"
+}
+
 locals {
   bucket_name = data.dotenv.dev_config.env.BUCKET_NAME
 }
