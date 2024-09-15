0e1f151f7e906fe30894a9ec1d1f0d014b51aab4
klakli <kkborowy@gmail.com>
2024-09-15 16:15:59 +0200

    creating a db, schemas, table and a file format

---
+++ terraform/03_sf_aws/sf_setup.tf
@@ -0,0 +1,36 @@
+resource "snowflake_database" "db" {
+    name = "DB"
+    comment = "A database for recent and previous tables"
+}
+
+resource "snowflake_schema" "recent_schema" {
+    name = "RECENT"
+    database = snowflake_database.db.name
+}
+
+resource "snowflake_schema" "previous_schema" {
+    name = "PREVIOUS"
+    database = snowflake_database.db.name
+}
+
+resource "snowflake_table" "raw" {
+    database = snowflake_database.db.name
+    schema = snowflake_schema.recent_schema.name
+    name = "raw_table"
+    column {
+      name = "raw_object"
+      type = "VARIANT"
+    }
+    lifecycle {
+      create_before_destroy = true # == create or replace
+    }
+}
+
+resource "snowflake_file_format" "csv_format" {
+    name = "csv_format" 
+    database = snowflake_database.db.name
+    schema = snowflake_schema.recent_schema.name
+    format_type = "CSV"
+    field_delimiter = ","
+}
+
