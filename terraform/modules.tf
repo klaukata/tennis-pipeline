2c6153f1f7c75049b3838158ddbc0dd388b4301e
klakli <kkborowy@gmail.com>
2024-09-18 12:53:47 +0200

    sf_setup module

--- terraform/modules.tf
+++ terraform/modules.tf
@@ -1,23 +1,28 @@
-module "m_01_s3" {
+module "m_s3" {
   source    = "./01_s3"
   s3_prefix = var.s3_prefix
 }
 
-module "m_02_aws" {
+module "m_aws" {
   source      = "./02_aws"
   bucket_name = local.bucket_name
 }
 
-module "m_03_sf_aws" {
-    # depends_on = [module.m_02_aws] #TODO 
-    source = "./03_sf_aws"
-    account_id = module.m_02_aws.account_id
-    role_name = module.m_02_aws.role_name
-    bucket_name = local.bucket_name
+module "m_sf_setup" {
+  source = "./03_sf_setup"
+  snow_acc = var.snowflake_account
+  snow_usr = var.snowflake_user
+  snow_pass = var.snowflake_user_password
+  snow_role = "ACCOUNTADMIN"
+}
 
-    # variable values defined in secret-variables.tf file
+module "m_sf_aws" {
+    source = "./04_sf_aws"
+    account_id = module.m_aws.account_id
+    role_name = module.m_aws.role_name
+    bucket_name = local.bucket_name
     snow_acc = var.snowflake_account
     snow_usr = var.snowflake_user
     snow_pass = var.snowflake_user_password
-    snow_role = snowflake_account_role.role.name
+    snow_role = "ACCOUNTADMIN"
 }
