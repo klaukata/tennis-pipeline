3b2399ac5abbb26e73366ba4a7bbc987e000d13a
klakli <kkborowy@gmail.com>
2024-09-18 12:58:39 +0200

    folder with a role and warehouse creation in snowflake

---
+++ terraform/03_sf_setup/main.tf
@@ -0,0 +1,15 @@
+terraform {
+  required_providers {
+    snowflake = {
+      source  = "Snowflake-Labs/snowflake"
+      version = "0.95.0"
+    }
+  }
+}
+
+provider "snowflake" {
+  account   = var.snow_acc
+  user      = var.snow_usr
+  password  = var.snow_pass
+  role      = var.snow_role
+}
---
+++ terraform/03_sf_setup/variables.tf
@@ -0,0 +1,15 @@
+variable "snow_acc" {
+  type = string
+}
+
+variable "snow_usr" {
+  type = string
+}
+
+variable "snow_pass" {
+  type = string
+}
+
+variable "snow_role" {
+  type = string
+}
