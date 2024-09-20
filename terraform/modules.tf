3aed23e038dd0c6a8e1de15e30992f8b04a1b160
klakli <kkborowy@gmail.com>
2024-09-20 07:20:20 +0200

    changed module locations and excluded log-in variables

--- terraform/modules.tf
+++ terraform/modules.tf
@@ -1,28 +1,17 @@
 module "m_s3" {
-  source    = "./01_s3"
+  source    = "./m_s3"
   s3_prefix = var.s3_prefix
 }
 
 module "m_aws" {
-  source      = "./02_aws"
+  source      = "./m_aws"
   bucket_name = local.bucket_name
-}
-
-module "m_sf_setup" {
-  source    = "./03_sf_setup"
-  snow_acc  = var.snowflake_account
-  snow_usr  = var.snowflake_user
-  snow_pass = var.snowflake_user_password
-  snow_role = "ACCOUNTADMIN"
 }
 
 module "m_sf_aws" {
-  source      = "./04_sf_aws"
+  source      = "./m_sf_aws"
   account_id  = module.m_aws.account_id
   role_name   = module.m_aws.role_name
   bucket_name = local.bucket_name
-  snow_acc    = var.snowflake_account
-  snow_usr    = var.snowflake_user
-  snow_pass   = var.snowflake_user_password
   snow_role   = "CUSTOMROLE"
 }
