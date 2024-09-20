4d1bafd254a9cb10623120abab0f9cdf208c4c40
klakli <kkborowy@gmail.com>
2024-09-20 07:23:15 +0200

    excluded log-in vars, and rm snow providers

--- terraform/main.tf
+++ terraform/main.tf
@@ -8,14 +8,6 @@ terraform {
       source  = "jrhouston/dotenv"
       version = "~> 1.0"
     }
-    snowflake = {
-      source  = "Snowflake-Labs/snowflake"
-      version = "0.95.0"
-    }
-    snowsql = {
-      source  = "aidanmelen/snowsql"
-      version = "1.3.3"
-    }
   }
 }
 
@@ -27,28 +19,4 @@ provider "dotenv" {}
 
 data "dotenv" "dev_config" {
   filename = ".env"
-}
-
-provider "snowsql" {
-  account   = var.snowflake_account
-  username  = var.snowflake_user
-  password  = var.snowflake_user_password
-  role      = "CUSTOMROLE"
-  warehouse = "WH"
-}
-
-# copy from an ext. stage to a table
-resource "snowsql_exec" "copy_table" {
-  create {
-    statements = <<-EOF
-      create role if not exists temprole;
-      COPY INTO DB.RECENT."raw_table"
-        from @DB.RECENT.STAGE
-        file_format = (FORMAT_NAME = DB.RECENT.CSVFORMAT)
-        on_error = "continue";
-    EOF
-  }
-  delete {
-    statements = "drop role if exists temprole"
-  }
 }
