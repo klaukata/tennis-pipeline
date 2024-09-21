c43d046c5e0af32bb27f89529e86343fde0616f9
klakli <kkborowy@gmail.com>
2024-09-21 10:18:33 +0200

    hides input for SNOWFLAKE_PASSWORD env var

--- setup_scripts/snowflake_env.sh
+++ setup_scripts/snowflake_env.sh
@@ -6,9 +6,9 @@ export SNOWFLAKE_ACCOUNT="$account"
 read "user?Enter a value for SNOWFLAKE_USER environment variable: "
 export SNOWFLAKE_USER="$user"
 
-read "password?Enter a value for SNOWFLAKE_PASSWORD environment variable: "
+read -s "password?Enter a value for SNOWFLAKE_PASSWORD environment variable: "
 export SNOWFLAKE_PASSWORD="$password"
 
 export SNOWFLAKE_ROLE="ACCOUNTADMIN"
 
-echo "Envionment variables have been set succesfully"
+echo "\nEnvionment variables have been set succesfully"
