f53ed2194e940c8d48183c8327c689cdc36dd87e
klakli <kkborowy@gmail.com>
2024-10-21 02:47:46 +0200

    s3_name var

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,7 +1,7 @@
-variable "s3_prefix" {
+variable "s3_name" {
   type        = string
   description = "Bucket prefix for our S3 bucket"
-  default     = "pipeline-bucket-"
+  default     = "pipeline-bucket"
 }
 
 variable "snowflake_user_role" {
@@ -11,5 +11,5 @@ variable "snowflake_user_role" {
 }
 
 locals {
-  bucket_name = data.dotenv.dev_config.env.BUCKET_NAME
+  account_id = data.aws_caller_identity.current.account_id
 }
