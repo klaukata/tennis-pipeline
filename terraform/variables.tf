6314b863c3211e92cc070b47991d2661b86906a0
klakli <kkborowy@gmail.com>
2024-11-15 18:28:22 +0100

    bucket_name variable

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,8 +1,7 @@
 # ____________VARS AND LOCALS____________
-variable "s3_name" {
+variable "bucket_name" {
   type        = string
-  description = "Bucket prefix for our S3 bucket"
-  default     = "pipeline_bucket"
+  description = "Name for our S3 bucket"
 }
 
 variable "snowflake_user_role" {
@@ -17,9 +16,7 @@ locals {
 }
 
 # ____________OUTPUTS____________
-output "bucket_name" {
-  value = var.s3_name
-}
+
 
 output "account_id" {
   value = local.account_id
