13caccdf15c03ac45db88a3ce7c5a594f900e563
klakli <kkborowy@gmail.com>
2024-11-15 18:27:47 +0100

    excluded 'dotenv' provider

--- terraform/main.tf
+++ terraform/main.tf
@@ -4,12 +4,8 @@ terraform {
       source  = "hashicorp/aws"
       version = "~> 5.0"
     }
-    dotenv = {
-      source  = "jrhouston/dotenv"
-      version = "~> 1.0"
-    }
   }
 }
 
 # connecting to an aws acc
-provider "aws" {}
+provider aws {}
