d52de77f07527c470428c194582a5c30d2b71605
klakli <kkborowy@gmail.com>
2025-01-09 19:39:46 +0100

    a dag that moves a file from s3 to sf!!!

--- astro_airflow/dags/main_dag.py
+++ astro_airflow/dags/main_dag.py
@@ -1,10 +1,11 @@
 import os
 from datetime import datetime
 
-from airflow.decorators import dag
+from airflow.decorators import dag, task
 from airflow.operators.empty import EmptyOperator
+from airflow.providers.snowflake.transfers.copy_into_snowflake import CopyFromExternalStageToSnowflakeOperator
 
-from cosmos import DbtDag, DbtTaskGroup, ProjectConfig, ProfileConfig, ExecutionConfig, RenderConfig
+from cosmos import DbtTaskGroup, ProjectConfig, ProfileConfig, ExecutionConfig, RenderConfig
 from cosmos.profiles import SnowflakeUserPasswordProfileMapping
 from cosmos.constants import SourceRenderingBehavior
 
@@ -19,27 +20,44 @@ profile_config = ProfileConfig(
 
 @dag(
     schedule_interval="@weekly",
+    template_searchpath="/usr/local/airflow/include",
     start_date=datetime(2024, 12, 23), #TODO
     catchup=False,
 )
-def main_dag():
-    pre_dbt = EmptyOperator(task_id="pre_dbt")
-
-    transform_and_test = DbtTaskGroup(
-        group_id='transform_and_test',
-        profile_config=profile_config,
-        project_config=ProjectConfig(
-            dbt_project_path="/usr/local/airflow/dags/dbt",
-        ),
-        execution_config=ExecutionConfig(
-            dbt_executable_path=f"{os.environ['AIRFLOW_HOME']}/dbt_venv/bin/dbt",
-        ),
-        render_config=RenderConfig(
-            source_rendering_behavior=SourceRenderingBehavior.WITH_TESTS_OR_FRESHNESS,
-        ),
-        operator_args={"install_deps": True},
+def move_file_from_s3():
+    copy_into = CopyFromExternalStageToSnowflakeOperator(
+        task_id='sts_sql',
+        snowflake_conn_id='snowflake_conn',
+        schema='RECENT',
+        table='RAW_TABLE',
+        stage='STAGE',
+        file_format='CSVFORMAT'
     )
+    copy_into
+move_file_from_s3()
+# def main_dag():
+#     @task.bash
+#     def upload_to_sf():
+#         return "ls -al /home/astro/."
+    
+#     upload_to_sf = upload_to_sf()
 
-    pre_dbt >> transform_and_test
+#     # transform_and_test = DbtTaskGroup(
+#     #     group_id='transform_and_test',
+#     #     profile_config=profile_config,
+#     #     project_config=ProjectConfig(
+#     #         dbt_project_path="/usr/local/airflow/dags/dbt",
+#     #     ),
+#     #     execution_config=ExecutionConfig(
+#     #         dbt_executable_path=f"{os.environ['AIRFLOW_HOME']}/dbt_venv/bin/dbt",
+#     #     ),
+#     #     render_config=RenderConfig(
+#     #         source_rendering_behavior=SourceRenderingBehavior.WITH_TESTS_OR_FRESHNESS,
+#     #     ),
+#     #     operator_args={"install_deps": True},
+#     # )
 
-main_dag()
+#     upload_to_sf 
+#     #>> transform_and_test
+
+# main_dag()
