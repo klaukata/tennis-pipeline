070152dfb7772c364e0b6aa12e4b002ad6ed392f
klakli <kkborowy@gmail.com>
2025-01-10 22:05:52 +0100

    moved files from root/airflow/tasks

---
+++ astro_airflow/plugins/uploader.py
@@ -0,0 +1,22 @@
+import boto3
+import os
+from dotenv import load_dotenv
+
+def get_env_var(var: str):
+    load_dotenv(dotenv_path='vars.env')
+    return os.getenv(var)
+
+def upload_to_s3(bucket_name: str, file_name: str, path: str) -> None:
+    s3 = boto3.resource('s3')
+    try:
+        s3.Bucket(bucket_name).upload_file(path, file_name)
+    except Exception as e:
+        print(f'Failed to upload {file_name} to a S3 bucket, because of an error: {e}')
+
+if __name__ == '__main__':
+    bucket_name = get_env_var('TF_VAR_bucket_name')
+    upload_to_s3(
+        bucket_name = bucket_name,
+        file_name = 'raw_data.csv',
+        path = '/tmp/raw_data.csv'
+    )
