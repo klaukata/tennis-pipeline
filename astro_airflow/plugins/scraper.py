56bd947b54cbdce7547813a088fb07383d749c12
klakli <kkborowy@gmail.com>
2025-01-24 17:24:34 +0100

    made chrome use /tmp instead of /dev/shm because of constant page crashing

--- astro_airflow/plugins/scraper.py
+++ astro_airflow/plugins/scraper.py
@@ -15,6 +15,7 @@ def init_driver():
     chrome_options = Options()
     chrome_options.add_argument('--no-sandbox')
     chrome_options.add_argument('--headless')
+    chrome_options.add_argument('--disable-dev-shm-usage')  # force chrome to use /tmp dir
 
     chrome_service = Service(ChromeDriverManager().install())
 
